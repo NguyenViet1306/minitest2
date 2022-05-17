088ee2eabf4c3527eca56912fb78c075ed80b577
NguyenViet1306 <vietsongtu@gmail.com>
2022-05-17 16:45:38 +0700

    add

--- info.js
+++ info.js
@@ -6,3 +6,19 @@ function creatArray() {
         array[i] = prompt("Nhập vào phần tử thứ " + (i+1))
     }
 }
+function songuyento() {
+    let sum = 0
+    for ( let i=0 ; i<500 ; i++ ) {
+        let flag = true
+        for ( let j=2 ; j<Math.sqrt(i); j++) {
+            if (i%j===0) {
+                flag = false
+                break
+            }
+        }
+        if (flag) {
+            sum += i
+        }
+        console.log(sum)
+    }
+}
