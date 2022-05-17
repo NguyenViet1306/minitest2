00266bc14c43607fce39f670a09eb180b1ad789a
NguyenViet1306 <vietsongtu@gmail.com>
2022-05-17 16:23:10 +0700

    add

--- bai3.js
+++ bai3.js
@@ -1,18 +1,15 @@
-let mun3 = [2, 3, 5, 7, 10, 11, 13]
+let num3 = [2, 3, 5, 7, 10, 11, 13]
 let tong = 0
-for ( let i=0 ; i<mun3.length ; i++) {
+for ( let i=0 ; i<num3.length ; i++) {
     let flog = true
-    for (let j = 0; j < mun3[i]; j++) {
-        if (mun3[i] % j === 0) {
+    for (let j = 2; j < Math.sqrt(num3[i]); j++) {
+        if (num3[i] % j === 0) {
             flog = false
             break
-            console.log(mun[i])
-        } else {
-            flog = true
-
         }
-
     }
-    tong += mun3[i]
-    // console.log("tống số nguyên tố là: " + tong)
+        if (flog) {
+            tong += num3[i]
+            }
+    console.log("tống số nguyên tố là: " + tong)
 }
